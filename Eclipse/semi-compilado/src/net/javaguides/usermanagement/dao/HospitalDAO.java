97dbdb64a1b0031387e04f4a6b9ca5fe140b9156
APF2000 <arthurpfonseca@gmail.com>
2020-11-27 22:35:13 -0300

    Comecando a infraestrutura

---
+++ Eclipse/semi-compilado/src/net/javaguides/usermanagement/dao/HospitalDAO.java
@@ -0,0 +1,13 @@
+package net.javaguides.usermanagement.dao;
+
+import java.util.List;
+
+public class HospitalDAO {
+
+	public List<String> selectCidades() {
+		// TODO Auto-generated method stub
+		List<String> ad_hoc;
+		return ad_hoc;
+	}
+
+}
