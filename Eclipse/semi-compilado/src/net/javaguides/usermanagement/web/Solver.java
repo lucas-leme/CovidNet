9b8b0fdd8f6b21c4593318b65bd6a52fba78fba8
APF2000 <arthurpfonseca@gmail.com>
2020-12-01 10:52:38 -0300

    Padrao de cpf funcionando e testado

--- Eclipse/semi-compilado/src/net/javaguides/usermanagement/web/Solver.java
+++ Eclipse/semi-compilado/src/net/javaguides/usermanagement/web/Solver.java
@@ -1,13 +1,17 @@
 package net.javaguides.usermanagement.web;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Solver {
 
 	// Verificar formato de cpf e digito verificador
 	public static boolean isCpfOk(String cpf)
 	{
-	    Pattern pattern = Pattern.compile("(\d{3}\.?){2}\d{3}\-?\d{2}");//, Pattern.CASE_INSENSITIVE);
-	    Matcher matcher = pattern.matcher("Solver de CPF");
+	    Pattern pattern = Pattern.compile("(\\d{3}\\.?){2}\\d{3}\\-?\\d{2}");//, Pattern.CASE_INSENSITIVE);
+	    Matcher matcher = pattern.matcher(cpf);
 	    boolean matchFound = matcher.find();
+	    
 	    if(matchFound) {
 	      System.out.println("Match found");
 	    } else {
@@ -27,6 +31,11 @@ public class Solver {
 	public static boolean isNameOk(String name)
 	{
 		return false;
+	}
+	
+	public static void main(String[] args)
+	{
+		System.out.println("CPF OK ? " + isCpfOk("123.456.789-00"));
 	}
 	
 }
