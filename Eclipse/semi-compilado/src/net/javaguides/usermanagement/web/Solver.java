ea91f3a1632b39938a04f6f1f7f196cc116194e8
APF2000 <arthurpfonseca@gmail.com>
2020-12-01 10:42:46 -0300

    Padrao para cpf

--- Eclipse/semi-compilado/src/net/javaguides/usermanagement/web/Solver.java
+++ Eclipse/semi-compilado/src/net/javaguides/usermanagement/web/Solver.java
@@ -5,6 +5,15 @@ public class Solver {
 	// Verificar formato de cpf e digito verificador
 	public static boolean isCpfOk(String cpf)
 	{
+	    Pattern pattern = Pattern.compile("(\d{3}\.?){2}\d{3}\-?\d{2}");//, Pattern.CASE_INSENSITIVE);
+	    Matcher matcher = pattern.matcher("Solver de CPF");
+	    boolean matchFound = matcher.find();
+	    if(matchFound) {
+	      System.out.println("Match found");
+	    } else {
+	      System.out.println("Match not found");
+	    }
+		
 		return false;
 	}
 	
