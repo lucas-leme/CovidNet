7e1b2306b49972bd13f01cffa57c0e333bdf8e3c
APF2000 <arthurpfonseca@gmail.com>
2020-12-01 10:24:45 -0300

    Criando solver

---
+++ Eclipse/semi-compilado/src/net/javaguides/usermanagement/web/Solver.java
@@ -0,0 +1,23 @@
+package net.javaguides.usermanagement.web;
+
+public class Solver {
+
+	// Verificar formato de cpf e digito verificador
+	public static boolean isCpfOk(String cpf)
+	{
+		return false;
+	}
+	
+	// Verificar formatacao de data
+	public static boolean isDateOk(String date)
+	{
+		return false;
+	}
+	
+	// Verificar nomes de pessoas, ruas, etc
+	public static boolean isNameOk(String name)
+	{
+		return false;
+	}
+	
+}
