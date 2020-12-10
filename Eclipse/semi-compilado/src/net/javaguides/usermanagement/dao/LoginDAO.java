b5fdc800b80ff646cd53724e36852ef15c73f077
Hugo Martins <hmartins19@gmail.com>
2020-12-10 17:09:13 -0300

    melhorias

--- Eclipse/semi-compilado/src/net/javaguides/usermanagement/dao/LoginDAO.java
+++ Eclipse/semi-compilado/src/net/javaguides/usermanagement/dao/LoginDAO.java
@@ -5,11 +5,6 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-
-import net.javaguides.usermanagement.model.Leito;
-import net.javaguides.usermanagement.model.Paciente;
 
 /**
  * 
