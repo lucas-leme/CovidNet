61d518e6aee338b96e211793c52fc9d9f6f7df28
hmartinsc <52382863+hmartinsc@users.noreply.github.com>
2020-12-10 17:17:14 -0300

    login (#60)
    * login
    
    * melhorias

--- Eclipse/semi-compilado/src/net/javaguides/usermanagement/dao/LoginDAO.java
+++ Eclipse/semi-compilado/src/net/javaguides/usermanagement/dao/LoginDAO.java
@@ -5,10 +5,6 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-
-import net.javaguides.usermanagement.model.Leito;
 
 /**
  * 
@@ -21,7 +17,7 @@ public class LoginDAO {
 	private String jdbcUsername = "g1";
 	private String jdbcPassword = "1HMUgvW";
 
-	private static final String SELECT_USER_AND_PSSWD = "SELECT";
+	private static final String SELECT_USER_AND_PSSWD = "SELECT cargo FROM funcionarios WHERE login = ? AND senha = ?";
 
 	protected Connection getConnection() {
 		Connection connection = null;
@@ -35,8 +31,28 @@ public class LoginDAO {
 			e.printStackTrace();
 		}
 		return connection;
-	}	
+	}
+	
+	public String loginUsuario(String login, String senha) {
+		String cargo = null;
+		
+		try (Connection connection = getConnection();
+				PreparedStatement preparedStatement = connection.prepareStatement(SELECT_USER_AND_PSSWD);) {
+			preparedStatement.setString(1, login);
+			preparedStatement.setString(2, senha);
+			System.out.println(preparedStatement);
+			ResultSet rs = preparedStatement.executeQuery();
+			
+			rs.first();
 
+			cargo = rs.getString("cargo");
+			
+		} catch (SQLException e) {
+			printSQLException(e);
+		}
+		return cargo;
+	}
+	
 	private void printSQLException(SQLException ex) {
 		
 		for (Throwable e : ex) {
